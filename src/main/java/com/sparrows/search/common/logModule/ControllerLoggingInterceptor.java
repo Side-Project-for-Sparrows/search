41ef225345ea7fb3c722265461a5f3d6a81d2f03
unstoppableworm <unstoppableworm@gmail.com>
2025-06-14 14:44:56 +0900

    로거 변경

--- src/main/java/com/sparrows/search/common/logModule/ControllerLoggingInterceptor.java
+++ src/main/java/com/sparrows/search/common/logModule/ControllerLoggingInterceptor.java
@@ -4,6 +4,7 @@ import com.sparrows.search.common.logModule.enums.TraceHeader;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -16,32 +17,39 @@ import java.util.UUID;
 public class ControllerLoggingInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String tid = Optional.ofNullable(request.getHeader(TraceHeader.X_TRACE_ID.key())).orElse(UUID.randomUUID().toString());
-        String pid = Optional.ofNullable(request.getHeader(TraceHeader.X_PARENT_SPAN_ID.key())).orElse(TraceHeader.ROOT.key());
-        String cid = UUID.randomUUID().toString(); // 현재 서비스 기준 새로운 CID
-
-        request.setAttribute(TraceHeader.X_TRACE_ID.key(), tid);
-        request.setAttribute(TraceHeader.X_PARENT_SPAN_ID.key(), pid);
-        request.setAttribute(TraceHeader.X_SPAN_ID.key(), cid);
-
         if (handler instanceof HandlerMethod handlerMethod) {
-            log.info("[INBOUND] [{}] [tid={}] [pid={}] [cid={}] 요청 수신",
-                    handlerMethod.getMethod().getDeclaringClass().getSimpleName() + "." +
-                            handlerMethod.getMethod().getName(), tid, pid, cid);
-        }
+            String tid = Optional.ofNullable(request.getHeader(TraceHeader.X_TRACE_ID.key())).orElse(UUID.randomUUID().toString());
+            String pid = Optional.ofNullable(request.getHeader(TraceHeader.X_PARENT_SPAN_ID.key())).orElse(TraceHeader.ROOT.key());
+            String cid = UUID.randomUUID().toString(); // 현재 서비스 기준 새로운 CID
+            String method = handlerMethod.getMethod().getName();
 
+            request.setAttribute(TraceHeader.X_TRACE_ID.key(), tid);
+            request.setAttribute(TraceHeader.X_PARENT_SPAN_ID.key(), pid);
+            request.setAttribute(TraceHeader.X_SPAN_ID.key(), cid);
+
+            MDC.put(TraceHeader.X_TRACE_ID.key(), tid);
+            MDC.put(TraceHeader.X_PARENT_SPAN_ID.key(), pid);
+            MDC.put(TraceHeader.X_SPAN_ID.key(), cid);
+            MDC.put("METHOD",method);
+            MDC.put("flow", "IN");
+
+            return true;
+        }
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         if (handler instanceof HandlerMethod handlerMethod) {
-            log.info("[OUTBOUND] [{}] [tid={}] [pid={}] [cid={}] 응답 완료",
-                    handlerMethod.getMethod().getDeclaringClass().getSimpleName() + "." +
-                            handlerMethod.getMethod().getName(),
-                    request.getAttribute(TraceHeader.X_TRACE_ID.key()),
-                    request.getAttribute(TraceHeader.X_PARENT_SPAN_ID.key()),
-                    request.getAttribute(TraceHeader.X_SPAN_ID.key()));
+            String tid = (String) request.getAttribute(TraceHeader.X_TRACE_ID.key());
+            String pid = (String) request.getAttribute(TraceHeader.X_PARENT_SPAN_ID.key());
+            String cid = (String) request.getAttribute(TraceHeader.X_SPAN_ID.key());
+
+            MDC.put(TraceHeader.X_TRACE_ID.key(),tid);
+            MDC.put(TraceHeader.X_PARENT_SPAN_ID.key(),pid);
+            MDC.put(TraceHeader.X_SPAN_ID.key(),cid);
+            MDC.put("METHOD",handlerMethod.getMethod().getName());
+            MDC.put("flow","OUT");
         }
     }
 }
